efa9aaccd4cfaca8da805827cc596914bc5c33bf
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-11 20:40:22 +0530

    Delete Change Column Names.sql

--- Change Column Names.sql
+++
@@ -1,6 +0,0 @@
---To Display the data types of the columns
-DESCRIBE coffee_shop_sales;
-
---CHANGE COLUMN NAME `ï»¿transaction_id` to transaction_id
-ALTER TABLE coffee_shop_sales
-CHANGE COLUMN `ï»¿transaction_id` transaction_id INT;
