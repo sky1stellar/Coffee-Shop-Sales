7d3e819266a74762757d1d75f88d5367602ad36c
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-11 20:10:42 +0530

    Update Changing Data Types.sql

--- scripts/Changing Data Types.sql
+++ scripts/Changing Data Types.sql
@@ -1,11 +1,18 @@
 -- Changing Data Types
 
 --CONVERT DATE (transaction_date) COLUMN TO PROPER DATE FORMAT
-
 UPDATE coffee_shop_sales
 SET transaction_date = STR_TO_DATE(transaction_date, '%d-%m-%Y');
 
 --ALTER DATE (transaction_date) COLUMN TO DATE DATA TYPE
-
 ALTER TABLE coffee_shop_sales
 MODIFY COLUMN transaction_date DATE;
+
+--CONVERT TIME (transaction_time)  COLUMN TO PROPER DATE FORMAT
+UPDATE coffee_shop_sales
+SET transaction_time = STR_TO_DATE(transaction_time, '%H:%i:%s');
+
+--ALTER TIME (transaction_time) COLUMN TO DATE DATA TYPE
+ALTER TABLE coffee_shop_sales
+MODIFY COLUMN transaction_time TIME;
+
