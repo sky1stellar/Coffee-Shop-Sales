b4150977fad6c3de719767ff3518e3eec51cd386
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-11 20:50:49 +0530

    Create Total Orders KPI - MOM Difference and MOM Growth.sql

---
+++ scripts/Total Orders KPI - MOM Difference and MOM Growth.sql
@@ -0,0 +1,14 @@
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(COUNT(transaction_id)) AS total_orders,
+    (COUNT(transaction_id) - LAG(COUNT(transaction_id), 1) 
+    OVER (ORDER BY MONTH(transaction_date))) / LAG(COUNT(transaction_id), 1) 
+    OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5) -- for April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
