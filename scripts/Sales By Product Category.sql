40ed33f623a931bbd2c996cb05aae4d61da631b9
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 20:09:35 +0530

    Create Sales By Product Category.sql

---
+++ scripts/Sales By Product Category.sql
@@ -0,0 +1,8 @@
+SELECT 
+	product_category,
+	ROUND(SUM(unit_price * transaction_qty),1) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) = 5 
+GROUP BY product_category
+ORDER BY SUM(unit_price * transaction_qty) DESC
