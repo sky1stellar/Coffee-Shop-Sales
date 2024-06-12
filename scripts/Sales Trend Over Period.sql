9d33ad7b199d8d5decc81f739d70201b529a94ff
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 19:57:04 +0530

    Create Sales Trend Over Period.sql

---
+++ scripts/Sales Trend Over Period.sql
@@ -0,0 +1,11 @@
+SELECT AVG(total_sales) AS average_sales
+FROM (
+    SELECT 
+        SUM(unit_price * transaction_qty) AS total_sales
+    FROM 
+        coffee_shop_sales
+	WHERE 
+        MONTH(transaction_date) = 5  -- Filter for May
+    GROUP BY 
+        transaction_date
+) AS internal_query;
