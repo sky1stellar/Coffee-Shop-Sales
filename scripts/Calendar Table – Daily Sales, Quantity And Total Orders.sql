587523d4418ee42bf4d35987888d49533d5f072c
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 19:55:30 +0530

    Create Calendar Table – Daily Sales, Quantity And Total Orders.sql

---
+++ scripts/Calendar Table – Daily Sales, Quantity And Total Orders.sql
@@ -0,0 +1,18 @@
+SELECT
+    SUM(unit_price * transaction_qty) AS total_sales,
+    SUM(transaction_qty) AS total_quantity_sold,
+    COUNT(transaction_id) AS total_orders
+FROM 
+    coffee_shop_sales
+WHERE 
+    transaction_date = '2023-05-18'; --For 18 May 2023
+
+--If you want to get exact Rounded off values then use below query to get the result:
+SELECT 
+    CONCAT(ROUND(SUM(unit_price * transaction_qty) / 1000, 1),'K') AS total_sales,
+    CONCAT(ROUND(COUNT(transaction_id) / 1000, 1),'K') AS total_orders,
+    CONCAT(ROUND(SUM(transaction_qty) / 1000, 1),'K') AS total_quantity_sold
+FROM 
+    coffee_shop_sales
+WHERE 
+    transaction_date = '2023-05-18'; --For 18 May 2023
