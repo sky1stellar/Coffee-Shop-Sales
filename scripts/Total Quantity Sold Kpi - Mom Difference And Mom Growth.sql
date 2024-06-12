76ccdc5eb159fe2f91a87863a8406affad8534b5
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 19:52:31 +0530

    Create Total Quantity Sold Kpi - Mom Difference And Mom Growth.sql

---
+++ scripts/Total Quantity Sold Kpi - Mom Difference And Mom Growth.sql
@@ -0,0 +1,14 @@
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(SUM(transaction_qty)) AS total_quantity_sold,
+    (SUM(transaction_qty) - LAG(SUM(transaction_qty), 1) 
+    OVER (ORDER BY MONTH(transaction_date))) / LAG(SUM(transaction_qty), 1) 
+    OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5)   -- for April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
