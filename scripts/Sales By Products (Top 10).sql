f08ae718396648c5f0c92fa67cba365648471c06
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 20:11:43 +0530

    Create Sales By Products (Top 10).sql

---
+++ scripts/Sales By Products (Top 10).sql
@@ -0,0 +1,9 @@
+SELECT 
+	product_type,
+	ROUND(SUM(unit_price * transaction_qty),1) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) = 5 
+GROUP BY product_type
+ORDER BY SUM(unit_price * transaction_qty) DESC
+LIMIT 10
