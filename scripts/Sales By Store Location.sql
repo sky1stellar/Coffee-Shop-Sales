f578fb4dd90ae581bb95e1aa7e5f09cb6d6dd425
sky1stellar <55621529+sky1stellar@users.noreply.github.com>
2024-06-12 20:08:39 +0530

    Create Sales By Store Location.sql

---
+++ scripts/Sales By Store Location.sql
@@ -0,0 +1,8 @@
+SELECT 
+	store_location,
+	SUM(unit_price * transaction_qty) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) =5 
+GROUP BY store_location
+ORDER BY 	SUM(unit_price * transaction_qty) DESC
